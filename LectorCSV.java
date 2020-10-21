9f8caaa5efecf712e5f0919b9da84b5624fccce5
CamiloAndresAcevedo <68291086+CamiloAndresAcevedo@users.noreply.github.com>
2020-10-21 10:19:33 -0500

    Add files via upload
    Este es un programa que muestra los promedios desde un archivo csv en una ventana usando un boton

--- LectorCSV.java
+++ LectorCSV.java
@@ -1,52 +1,69 @@
+//Programa que hace los promedios por estudiante y columna desde un archivo csv.
+//Creado por Camilo Acevedo
+//Inspirado en alejandro programacion.
+
 package main;
 
-import java.io.BufferedReader;
+import java.io.BufferedReader; //Importamos las clases para la ejecucion del programa.
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JTextArea;
 
 public class LectorCSV {
 
-    private final String SEPARATOR = ",";          //declaracion de una variable
-    private ArrayList<String[]> datos;          //creacion de un arreglo
+	private final String SEPARATOR = ","; //Definimos el separador del archivo a leer.
+    private ArrayList<String[]> datos; //creacion de un arreglo.
 
     public void extraerDatos(String url) {
 
-        datos = new ArrayList<>();          //creacion de un nuevo arreglo
-        BufferedReader br = null;          //lee un archivo csv
+        datos = new ArrayList<>(); //creacion de un nuevo arreglo
+        BufferedReader br = null; //leemos el texto de una secuencia de entrada
 
         try {
 
-            br = new BufferedReader(new FileReader(url));          //creacion de una clase para lectura de un archivo
-            String line = br.readLine();                  //declaracion de variables tipo texto
-            while (null != line) {                 //ciclo repetitivo hasta que acabe la operacion
+            br = new BufferedReader(new FileReader(url)); //creacion de una clase para lectura de un archivo
+            String line = br.readLine();  //declaracion de variables tipo texto
+            while (null != line) { //ciclo repetitivo hasta que acabe la operacion
                 String[] fields = line.split(SEPARATOR);
                 datos.add(fields);
                 line = br.readLine();
             }
 
-        } catch (Exception e) {                //contiene sentencias 
-            System.out.println(e);                //metodo para imprimir
+        } catch (Exception e) { //contiene sentencias 
+            System.out.println(e);  //metodo para imprimir
         } finally {
-            if (null != br) {                //metodo que proporciona instrucciones al programa
-                try {    //se utiliza para tener excepciones en el programa
-                    br.close();      //cierre de operacion
-                } catch (IOException ex) {              //contiene sentencias 
+            if (null != br) { //metodo que proporciona instrucciones al programa
+                try { //se utiliza para tener excepciones en el programa
+                    br.close();  //cierre de operacion
+                } catch (IOException ex) { //contiene sentencias 
                     Logger.getLogger(LectorCSV.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
         }
     }
 
-    public void calcularPromedio(JTextArea txa) {                //metodo principal del programa 
-        for (String[] x : datos) {     //ciclo repetitivo que recorre el programa hasta acabar todas las sentencias
-            txa.append("Cod. Estudiante: " + x[0]);                //el txa.append recolecta la info que se especifica, en este caso es el codigo del estudiante
-            txa.append("\nNombre: "+x[1]);                    //el txa.append recolecta la info que se especifica, en este caso es el nombre
-            int promedio = (Integer.parseInt(x[2])+Integer.parseInt(x[3])+Integer.parseInt(x[4])+Integer.parseInt(x[5]))/4;        //declara una variable tipo entera
-            txa.append("\nPromedio de notas: "+promedio+"\n\n");     //el txa.append recolecta la info que se especifica, en este caso es el resultado de promedio
+    public String[][] calcularPromedio() {
+        String[][] promedios = new String[datos.size()][2];
+        
+        for (int i = 0; i < datos.size(); i++) {
+            promedios[i][0] = datos.get(i)[1];
+            float prom = (Float.parseFloat(datos.get(i)[2]) + Float.parseFloat(datos.get(i)[3])
+                    + Float.parseFloat(datos.get(i)[4]) + Float.parseFloat(datos.get(i)[5])) / 4;
+            promedios[i][1] = prom + "";
         }
+        return promedios;
+    }
+    
+    public float getPromedioNota(int nota){
+        float promedio = 0;
+        int posicionNota = 1+nota;
+        for(String[] x : datos){
+            promedio+=Float.parseFloat(x[posicionNota]);
+        }
+        promedio/=datos.size();
+        
+        return promedio;
     }
 }
--- Ventana.java
+++ Ventana.java
@@ -1,48 +1,115 @@
+//Programa que muestra los promedios desde un archivo csv en una ventana usando un boton.
+//Creado por Camilo Acevedo
+//Inspirado en alejandro programacion.
+
 package main;
 
-import javax.swing.JFileChooser;
-import javax.swing.filechooser.FileNameExtensionFilter;
+import java.util.ArrayList; // import the ArrayList class
+import javax.swing.JFileChooser; //clase para mostrar una ventana para la selección de un fichero.
+import javax.swing.filechooser.FileNameExtensionFilter; //Implementación de filtros usando un conjunto específico de extensiones.
+import javax.swing.table.DefaultTableModel; //Implementación de TableModelque que usa un vector para almacenar los objetos de valor de celda.
 
 public class Ventana extends javax.swing.JFrame {
 
     public Ventana() {
-        initComponents();
+        initComponents(); //Se cargan los objetos del jFrame
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        File = new javax.swing.JFileChooser();
-        jButton1 = new javax.swing.JButton();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        txaLista = new javax.swing.JTextArea();
+        File = new javax.swing.JFileChooser(); //Mecanismo para que el usuario elija un archivo.
+        jButton1 = new javax.swing.JButton(); //Implementación de un botón "pulsador".
+        jScrollPane2 = new javax.swing.JScrollPane(); //Proporciona una vista desplazable de un componente.
+        Tabla = new javax.swing.JTable(); //Componente visual de java que nos permite dibujar una tabla.
+        jLabel1 = new javax.swing.JLabel(); //Mostramos los diferentes tipos de variables como texto.
+        lblPromedioTotal = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        PromN1 = new javax.swing.JLabel(); 
+        jLabel3 = new javax.swing.JLabel(); 
+        jLabel4 = new javax.swing.JLabel(); 
+        jLabel5 = new javax.swing.JLabel(); 
+        PromN2 = new javax.swing.JLabel();
+        PromN3 = new javax.swing.JLabel();
+        PromN4 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jButton1.setText("Abrir");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        jButton1.setText("Abrir"); //Texto que tendra el boton.
+        jButton1.addActionListener(new java.awt.event.ActionListener() { //Proceso para la ejecucion del boton.
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
 
-        txaLista.setColumns(20);
-        txaLista.setRows(5);
-        jScrollPane1.setViewportView(txaLista);
+        Tabla.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null},
+                {null, null},
+                {null, null},
+                {null, null}
+            },
+            new String [] {
+                "Estudiante", "Promedio" //Titulos para las columnas.
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false
+            };
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane2.setViewportView(Tabla); //Mostramos en la tabla los promedios por nota y un promedio general.
+
+        jLabel1.setText("Promedio Total:");
+
+        jLabel2.setText("Promedio Nota 1:");
+
+        jLabel3.setText("Promedio Nota 2:");
+
+        jLabel4.setText("Promedio Nota 3:");
+
+        jLabel5.setText("Promedio Nota 4:");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane()); //Configuramos la ventana, sus caracteristicas y su interior.
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1)
-                .addContainerGap())
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(302, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup() //Agregamos el contenido de la ventana.
+                .addContainerGap(250, Short.MAX_VALUE)
                 .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(293, 293, 293))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane2)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel1)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(lblPromedioTotal))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel2)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(PromN1))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel4)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(PromN3))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel5)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(PromN4))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel3)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(PromN2)))
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -50,37 +117,78 @@ public class Ventana extends javax.swing.JFrame {
                 .addContainerGap()
                 .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 459, Short.MAX_VALUE)
-                .addContainerGap())
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(lblPromedioTotal))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(PromN1))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(PromN2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4)
+                    .addComponent(PromN3))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel5)
+                    .addComponent(PromN4))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        FileNameExtensionFilter filtro = new FileNameExtensionFilter("CSV promedio", "csv");  
-        File.setFileFilter(filtro);                                   //se filtran los datos, para poderlos mostrar mas adelante 
-        int result = File.showOpenDialog(this);                     //se declara una variable tipo entera
-        if(result == JFileChooser.APPROVE_OPTION){                   //metodo que proporciona instrucciones al programa
-        LectorCSV lector = new LectorCSV();                   //lee fichero csv
-        lector.extraerDatos(File.getSelectedFile().getAbsolutePath());                   //metodo que extrae los datos del archivo csv
-        lector.calcularPromedio(txaLista);                               //metodo para calcular el promedio
+        FileNameExtensionFilter filtro = new FileNameExtensionFilter("Promedio CSV", "csv"); //Definimos la extension en formato CSV.
+        File.setFileFilter(filtro); // se filtran los datos para poderlos mostrar mas adelante.
+        int result = File.showOpenDialog(this); //se declara una variable tipo entera.
+        if (result == JFileChooser.APPROVE_OPTION) { //metodo que proporciona instrucciones al programa.
+            LectorCSV lector = new LectorCSV(); //lee fichero csv.
+            lector.extraerDatos(File.getSelectedFile().getAbsolutePath()); //metodo que extrae los datos del archivo csv
+            String[][] promedios = lector.calcularPromedio(); //Cadena de caracteres para el promedio.
+            String[] columnas = new String[]{"Estudiante","Promedio"}; //Definimos el titulo de las columnas en la ventana.
+            DefaultTableModel model = new DefaultTableModel(promedios, columnas); //Definimos los datos para cada fila/columna
+            Tabla.setModel(model);
+            float promedioTotal = 0; //Representamos un dato.
+            for(String[] x :promedios){
+                promedioTotal+=Float.parseFloat(x[1]);
+            }
+            promedioTotal /=promedios.length; //Leemos los promedios tanto por columna como el promedio total.
+            lblPromedioTotal.setText(promedioTotal+"");
+            PromN1.setText(lector.getPromedioNota(1)+""); 
+            PromN2.setText(lector.getPromedioNota(2)+"");
+            PromN3.setText(lector.getPromedioNota(3)+"");
+            PromN4.setText(lector.getPromedioNota(4)+"");
         }
     }//GEN-LAST:event_jButton1ActionPerformed
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
-        	
-        	
             public void run() {
-                new Ventana().setVisible(true);
+                new Ventana().setVisible(true); //Control de la ventana.
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JFileChooser File;
+    private javax.swing.JLabel PromN1;
+    private javax.swing.JLabel PromN2;
+    private javax.swing.JLabel PromN3;
+    private javax.swing.JLabel PromN4;
+    private javax.swing.JTable Tabla;
     private javax.swing.JButton jButton1;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextArea txaLista;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JLabel lblPromedioTotal;
     // End of variables declaration//GEN-END:variables
 }
